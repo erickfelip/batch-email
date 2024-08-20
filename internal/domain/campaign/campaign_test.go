a5eccf6c377aaa205044fc1476ecd1d0a81b2769
erickfelip <erickfelipe2010@gmail.com>
2024-08-20 20:53:38 -0300

    test: new campaign id is not null
    test: new campaign createdOn must be now
    
    feat: add xid as campaign id

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,19 +2,40 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_CreateNewCampaign(t *testing.T) {
 	assert := assert.New(t)
 	name := "Campaign X"
 	content := "Body"
 	contacts := []string{"email1@e.com", "email2@e.com"}
 
 	campaign := NewCampaign(name, content, contacts)
-	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_NewCampaign_IDIsNotNill(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Body"
+	contacts := []string{"email1@e.com", "email2@e.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedOnMustBeNow(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Body"
+	contacts := []string{"email1@e.com", "email2@e.com"}
+	now := time.Now().Add(-time.Minute)
+
+	campaign := NewCampaign(name, content, contacts)
+	assert.Greater(campaign.CreatedOn, now)
 }
