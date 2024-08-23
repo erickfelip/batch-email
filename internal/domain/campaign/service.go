a03ebbd506bf57a58229c1e45c3f039f91cc4bbe
erickfelip <erickfelipe2010@gmail.com>
2024-08-22 21:51:20 -0300

    test: validate domain error
    test:validate repository save

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -10,8 +10,17 @@ type Service struct {
 
 func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
 
-	campaign, _ := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
-	s.Repository.Save(campaign)
+	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+	// validando se houve erro na criação da campanha
+	if err != nil {
+		return "", err
+	}
+	// validando se houve erro no mock para salvar no "BD"
+	err = s.Repository.Save(campaign)
+	if err != nil {
+		return "", err
+	}
 
+	// não houve erro, campanha é criada e o ID da campanha é retornado
 	return campaign.ID, nil
 }
--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -2,6 +2,7 @@ package campaign
 
 import (
 	"batch-email/internal/contract"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,24 +19,31 @@ func (r *repositoryMock) Save(campaign *Campaign) error {
 	return args.Error(0)
 }
 
-func Test_Create_Campaign(t *testing.T) {
-	assert := assert.New(t)
-
-	//chamando service + repository
-	service := Service{}
-
-	//passando DTO
-	newCampaign := contract.NewCampaign{
+var (
+	newCampaign = contract.NewCampaign{
 		Name:    "Nome da campanha",
 		Content: "Body",
 		Emails:  []string{"teste@teste.com", "teste2@email.com"},
 	}
 
-	// simulando a criação da campanha
+	service = Service{}
+)
+
+func Test_Create_Campaign(t *testing.T) {
+	assert := assert.New(t)
 	id, err := service.Create(newCampaign)
 
 	assert.NotNil(id)
 	assert.Nil(err)
+}
+
+func Test_Create_ValidateDomainError(t *testing.T) {
+	assert := assert.New(t)
+	newCampaign.Name = ""
+	_, err := service.Create(newCampaign)
+
+	assert.NotNil(err)
+	assert.Equal("name is required", err.Error())
 }
 
 func Test_Create_Campaign_Save_On_Db(t *testing.T) {
@@ -57,11 +65,22 @@ func Test_Create_Campaign_Save_On_Db(t *testing.T) {
 		return true
 	})).Return(nil)
 
-	//chamando service + repository
-	service := Service{Repository: repositoryMock}
+	service.Repository = repositoryMock
 
 	// simulando a criação da campanha
 	service.Create(newCampaign)
 
 	repositoryMock.AssertExpectations(t)
+}
+
+func Test_Create_ValidateRepositorySave(t *testing.T) {
+	assert := assert.New(t)
+
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.Anything).Return(errors.New("error to save on database"))
+
+	service.Repository = repositoryMock
+
+	_, err := service.Create(newCampaign)
+	assert.Equal("error to save on database", err.Error())
 }
